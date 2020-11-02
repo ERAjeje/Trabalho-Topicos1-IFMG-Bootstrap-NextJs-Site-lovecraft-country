71a9198a6151d645b20561845e53efae18d4ea63
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-11-02 08:49:58 -0300

    alteração de correção de bugs

--- pages/components/carousel/Carousel.js
+++ pages/components/carousel/Carousel.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import { UncontrolledCarousel } from 'reactstrap';
 import css from './carousel.module.css'
 
 const items = [
-    {
-        src: 'https://www.youtube.com/watch?v=a7XlZJ4F3e0',
-        header: '1',
-        key: '1'
-    },
-    {
-        src: 'https://i.ibb.co/bgvWBFV/netflix-clone-tela.png',
-        header: '',
-        key: '2'
-    },
-    {
-        src: 'https://i.ibb.co/SPFNf8Y/netflix-clone-tela2.png',
-        header: '',
-        key: '3'
-    },
+   'Ambientada no universo de HP Lovecraft, a série mostra a jornada de Áticus descobrindo que os verdadeiros horrores a enfrentar não estão no sobrenatural.',
+   'Cthulhu... Shoggoths... Kumiho... Nenhum horror se compara ao verdadeiro horror da humanidade.',
+   'Utilizando de fatos históricos reais dos EUA, a showrunner Misha Green apresenta o verdadeiro monstro da humanidade.',
 ];
 
-const HomeCarousel = () => <UncontrolledCarousel items={items} className={css.carousel}/>;
+let index = 0;
 
-export default HomeCarousel;
+export default function HomeCarousel (){
+
+    const [frase, setFrase] = useState('');
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let aux = index
+            setFrase(items[index % 3]);
+            index = ++aux
+        }, 10000)
+        return () => {
+            clearInterval(interval);
+        }
+    }, [frase])
+    return(
+        <div className={css.divCarousel}>
+            <p>{frase}</p>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import HomeCarousel from './components/carousel/Carousel'
 
 export default function Home() {
     return (
@@ -36,7 +37,7 @@ export default function Home() {
                 <div className={styles.divTitle}>
                     <span className={styles.spanTitle}>Bem-vindo ao</span> <img className={styles.logoTitle} src='/assets/images/logo.png'/>
                 </div>
-
+                <HomeCarousel />
             </main>
             <Footer className={styles.footerMain}/>
         </div>
