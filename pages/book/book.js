d2a3f5543ecbd6a6f9db1f87003fa8cb0ab8858c
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-12-06 20:57:57 -0300

    Implementação pag book

--- pages/book/book.js
+++ pages/book/book.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:212e1b4b5332a218e33552d6d82d346628e6b6bd86241e07ed1f0632f0c02fe6
-size 3568
+oid sha256:0de213efdaf7af9664203a6c042e96fd2dfdba0fa8d1b55e1818487735f7d07c
+size 4222
