651972a1d31e76eb52034ec79c9284fcb11ebe95
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-12-06 20:57:28 -0300

    Implementação pag bio

--- pages/bio/bio.js
+++ pages/bio/bio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbb00f83a1cbe4d624a0916aa4f158ba5de626b7101c38358c1640d4f9f8f35d
-size 1216
+oid sha256:9a42e4da850d64a25720521924cc9dcd328d2815ae4e29d28da207f71df0d5b5
+size 6813
