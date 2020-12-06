ad240d95b3a21d569a225acbee63a661bc55f166
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-12-06 20:56:54 -0300

    Refat HomeCarousel e exclui hello.js

--- pages/api/hello.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2af2a2b6fde676184e741377d2fa62ac60d3fae071982eef5c4c617ed6490ce0
-size 168
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12c3e271297dd1436b72f506b184385640020dc1799df9fbe22eeef6f8b438c0
-size 1337
+oid sha256:f48d2771df1e138b6aabca1898389637110119d0b2cddecb28d92fe9ac1bc42d
+size 1340
