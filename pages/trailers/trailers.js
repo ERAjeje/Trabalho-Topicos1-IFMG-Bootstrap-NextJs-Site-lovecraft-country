691e4c454dcd322fd03e7a60b1e1a6364a6db060
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-12-06 21:09:32 -0300

    Implementação da page trailers

--- pages/trailers/trailers.js
+++ pages/trailers/trailers.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06d30afce3149103a5b8e02edef4ba852dcd3a0529be182f9c7923b38ce133f2
-size 1394
+oid sha256:7dc6e3abc929f1cff732a5e62c5551ccae0082059fedf1e4d1d215e86479f74e
+size 3421
