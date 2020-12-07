7e14a27121eac5b1769979a79847f010c1b266c1
ERAjeje <42541492+ERAjeje@users.noreply.github.com>
2020-12-06 21:08:17 -0300

    Implementação da page episodes

--- pages/episodes/episodes.js
+++ pages/episodes/episodes.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdba541eb1b0bcac5b1a15670ab63a5ebcaf2f1648f8c82f66cf55b047523c3b
-size 1394
+oid sha256:b5618c6a4f0686b27d92200e086bbf5bf9f1b00395bf00762fd70748e5648ed2
+size 6412
